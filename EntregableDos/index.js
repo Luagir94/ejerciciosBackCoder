0fa87c16335c73d634489cb6e1d4fa65e41214ee
Luciano Giraudi <lucianoagiraudi@gmail.com>
2022-02-21 02:44:56 -0300

    desafio dos

--- EntregableDos/index.js
+++ EntregableDos/index.js
@@ -6,18 +6,18 @@ let counter = 0
 
 class Contenedor {
     constructor(title, price, thumbnail) {
-            this.id = undefined,
+        this.id = undefined,
             this.title = title,
             this.price = price,
             this.thumbnail = thumbnail
     }
 
-    save =  (obj) => {
+    save = (obj) => {
         try {
             counter += 1
             this.id = counter
             products.push(obj)
-             fs.writeFileSync('./desafio.txt', `${JSON.stringify(products)}`)
+            fs.writeFileSync('./desafio.txt', `${JSON.stringify(products)}`)
             console.log('Archivo guardado!')
         } catch (error) {
             console.log(error)
@@ -25,7 +25,7 @@ class Contenedor {
     }
 
 
-    getByID = (id) => console.log('getById' , products.find(x => x.id === id) )
+    getByID = (id) => console.log('getById', products.find(x => x.id === id))
 
     getAll = () => console.log(products)
 
@@ -39,8 +39,8 @@ class Contenedor {
         }
     }
 
-    deleteAll = async () =>{
-        const newArray = products.splice(0,products.length)
+    deleteAll = async () => {
+        const newArray = products.splice(0, products.length)
         await fs.promises.writeFile('./desafio.txt', ``)
         console.log('Productos eliminados')
     }
@@ -50,11 +50,11 @@ class Contenedor {
 }
 
 // Instancio y guardo los productos nuevos
-const productoUno = new Contenedor('cartuchera' , 100, 'www.dsfsd.com')
+const productoUno = new Contenedor('cartuchera', 100, 'www.dsfsd.com')
 productoUno.save(productoUno)
-const productoDos = new Contenedor('goma' , 10, 'www.dsfsd.com')
+const productoDos = new Contenedor('goma', 10, 'www.dsfsd.com')
 productoDos.save(productoDos)
-const productoTres = new Contenedor('lapiz' , 20, 'www.dsfsd.com')
+const productoTres = new Contenedor('lapiz', 20, 'www.dsfsd.com')
 productoTres.save(productoTres)
 
 //Obtengo todos los productos
