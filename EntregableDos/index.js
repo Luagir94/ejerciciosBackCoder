963791f4fe8a2d9b390394074e6940e6780b355d
Luciano Giraudi <lucianoagiraudi@gmail.com>
2022-02-22 00:41:34 -0300

    correccion desafio 2

--- EntregableDos/index.js
+++ EntregableDos/index.js
@@ -1,33 +1,51 @@
 const fs = require('fs')
 
 
-const products = []
-let counter = 0
+
 
 class Contenedor {
-    constructor(title, price, thumbnail) {
-        this.id = undefined,
-            this.title = title,
-            this.price = price,
-            this.thumbnail = thumbnail
+    constructor() {
+            this.id = undefined,
+            this.title = undefined,
+            this.price = undefined,
+            this.thumbnail = undefined
     }
 
-    save = (obj) => {
-        try {
-            counter += 1
-            this.id = counter
-            products.push(obj)
-            fs.writeFileSync('./desafio.txt', `${JSON.stringify(products)}`)
+    save = (title, price, thumbnail, obj) => {
+            if (fs.existsSync('./desafio.txt')) {
+                const data = JSON.parse(fs.readFileSync('./desafio.txt', 'utf-8'))
+
+                const lastProd = data[data.length - 1].id
+                this.title = title
+                this.price = price
+                this.thumbnail = thumbnail
+                this.id = lastProd + 1
+                console.log(data.length)
+                data.push(obj)
+                console.log(data, 'aca')
+                fs.writeFileSync('./desafio.txt', `${JSON.stringify(data)}`)
+            } else {
+                const array = []
+                this.title = title
+                this.price = price
+                this.thumbnail = thumbnail
+                this.id = 1
+                array.push(obj)
+                fs.writeFileSync('./desafio.txt', `${JSON.stringify(array)}`)
+            }
             console.log('Archivo guardado!')
-        } catch (error) {
-            console.log(error)
-        }
     }
 
 
-    getByID = (id) => console.log('getById', products.find(x => x.id === id))
+    getByID = (id) => {
+        const data = fs.readFileSync('./desafio.txt', 'utf-8')
+        console.log('getById', JSON.parse(data).find(x => x.id === id))
+    }
 
-    getAll = () => console.log(products)
+    getAll = () => {
+        const data = fs.readFileSync('./desafio.txt', 'utf-8')
+        console.log(data)
+    }
 
     deleteById = async (id) => {
         try {
@@ -50,19 +68,16 @@ class Contenedor {
 }
 
 // Instancio y guardo los productos nuevos
-const productoUno = new Contenedor('cartuchera', 100, 'www.dsfsd.com')
-productoUno.save(productoUno)
-const productoDos = new Contenedor('goma', 10, 'www.dsfsd.com')
-productoDos.save(productoDos)
-const productoTres = new Contenedor('lapiz', 20, 'www.dsfsd.com')
-productoTres.save(productoTres)
-
+const contenedor = new Contenedor()
+contenedor.save('cartuchera', 100, 'www.dsfsd.com',contenedor)
+contenedor.save('lapiz', 20, 'www.dsfsdsss.com',contenedor)
+// contenedor.save('hoja', 5, 'www.dsfsd111.com',contenedor)
 //Obtengo todos los productos
-productoUno.getAll()
+// productoUno.getAll()
 //Obtengo el producto con ID 2
-productoUno.getByID(2)
+// productoUno.getByID(2)
 //Elimino el producto con ID 2
-productoUno.deleteById(2)
+// productoUno.deleteById(2)
 //Metodo para borrar todo
 // productoUno.deleteAll()
 
